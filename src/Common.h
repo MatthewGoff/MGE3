249f410c0b2a291b2c96404a9945dfb8d0d493e2
Matthew Goff <mtthwgff@gmail.com>
2021-03-23 11:06:34 -0400

    Added some macros

--- src/Common.h
+++ src/Common.h
@@ -17,6 +17,7 @@
  * 
  * Aliases
  * int32 -> int
+ * uint32 -> uint32
  * byte -> uchar
 */
 
@@ -26,7 +27,16 @@ typedef unsigned int uint;
 typedef unsigned long long uint64;
 typedef long long int64;
 typedef int int32;
+typedef uint uint32;
 typedef unsigned char byte;
+
+#define UINT32_MAX (0xFFFFFFFF)
+
+#define KILOBYTES (1024)
+#define MEGABYTES (1024 * KILOBYTES)
+#define GIGABYTES (1024 * MEGABYTES)
+
+#define Assert(expression) if(!(expression)) {*(int * )0 = 0;}
 
 #include <math.h>
 #include <stdio.h>
