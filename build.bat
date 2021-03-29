d514273c90932839d23b21b81287c81f5d21ea14
Matthew Goff <mtthwgff@gmail.com>
2021-03-29 12:12:23 -0400

    moved command_file into build.bat

--- build.bat
+++ build.bat
@@ -6,4 +6,41 @@ if not exist build\data mkdir build\data
 if exist build\bin rmdir /s /q build\bin
 mkdir build\bin
 
-cl @command_file.txt
+set build_output= -Febuild\bin\main.exe -Fobuild\bin\ -Fdbuild\bin\
+
+:: create debug information (for vs)
+set build_flags= -Zi
+
+set build_search= -I src\ -I src\Engine -I src\WP -I src\Engine\struct
+
+:: set build_libs=" user32.lib gdi32.lib winmm.lib"
+
+:: Libraries to include
+set build_libs=
+set build_libs=%build_libs% user32.lib
+set build_libs=%build_libs% gdi32.lib
+set build_libs=%build_libs% winmm.lib
+
+:: Header files to include everywhere
+set build_include=
+set build_include=%build_include% -FICommon.h
+set build_include=%build_include% -FIConfig.h
+set build_include=%build_include% -FIMemory.h
+set build_include=%build_include% -FIStandard\Standard.h
+set build_include=%build_include% -FImalloc.h
+set build_include=%build_include% -FImath.h
+
+:: source files to be compiled
+set build_source=
+set build_source=%build_source% src\WP\WinMain.cpp
+set build_source=%build_source% src\WP\Clock.cpp
+set build_source=%build_source% src\WP\Print.cpp
+set build_source=%build_source% src\Engine\GameMain.cpp
+set build_source=%build_source% src\Engine\OpenBitmap.cpp
+set build_source=%build_source% src\Standard\Util.cpp
+set build_source=%build_source% src\Engine\Rendering.cpp
+set build_source=%build_source% src\Standard\Vector.cpp
+set build_source=%build_source% src\Engine\AssetManager.cpp
+
+:: cl @command_file.txt
+cl%build_output%%gme_flags%%build_search%%build_libs%%build_include%%build_source%
