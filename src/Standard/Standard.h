363425de7b14c6157f208e31e34d843bfe96fab5
Matthew Goff <mtthwgff@gmail.com>
2021-04-08 12:37:03 -0400

    bikeshedding

--- src/Standard/Standard.h
+++ src/Standard/Standard.h
@@ -36,7 +36,7 @@ namespace String
 {
     char* ToString(char* buffer, int val);
     char* ToString(float val);
-    int Compare(char* one, char* two);
+    int Compare(char* s1, char* s2);
 }
 
 #endif
