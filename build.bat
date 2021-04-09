c32911300ff92ef06cd637dcca22931cea604488
Matthew Goff <mtthwgff@gmail.com>
2021-04-09 18:50:33 -0400

    suppressed one warning in build.bat

--- build.bat
+++ build.bat
@@ -12,6 +12,10 @@ set build_output= -Febuild\bin\main.exe -Fobuild\bin\ -Fdbuild\bin\
 :: -Zi => debug information (for VS)
 set build_flags= -Zi
 
+set skip_warnings=
+:: 4533 initialization of <var> is skipped by <goto>
+set skip_warnings=%skip_warnings% -wd4533
+
 set build_sdk_include= -I src\
 set build_sdk_include=%build_sdk_include% -I C:\GLM\glm\
 set build_sdk_include=%build_sdk_include% -I C:\VulkanSDK\1.2.162.1\Include\vulkan
@@ -51,4 +55,4 @@ set build_source=%build_source% src\Standard\String.cpp
 set build_source=%build_source% src\WP\Render.cpp
 
 :: cl @command_file.txt
-cl%build_output%%build_flags%%build_include%%build_libs%%build_sdk_include%%build_source%
+cl%build_output%%build_flags%%build_include%%skip_warnings%%build_libs%%build_sdk_include%%build_source%
