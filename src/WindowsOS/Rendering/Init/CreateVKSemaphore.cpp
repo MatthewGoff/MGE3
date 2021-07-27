460d0e677c8706e16114b16f7a26eed83bdf19a6
Matthew Goff <mtthwgff@gmail.com>
2021-07-27 13:25:44 -0400

    stable

--- src/WindowsOS/Rendering/Init/CreateVKSemaphore.cpp
+++ src/WindowsOS/Rendering/Init/CreateVKSemaphore.cpp
@@ -4,17 +4,17 @@ namespace WindowsOS { namespace Rendering {
 namespace Init
 {
     bool CreateVKSemaphore(
-        VkDevice logical_device_handle,
-        VkSemaphore* semaphore_handle)
+        VkDevice logical_device,
+        VkSemaphore* semaphore)
     {
         VkSemaphoreCreateInfo semaphore_info = {};
         semaphore_info.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
 
         VkResult result = vkCreateSemaphore(
-            logical_device_handle,
+            logical_device,
             &semaphore_info,
             nullptr,
-            semaphore_handle);
+            semaphore);
         if (result != VK_SUCCESS)
         {
             return false;
